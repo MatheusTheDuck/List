5368f7f6620eb7e62743ca4570069a52a0f8fc84
MatheusTheDuck <83176908+MatheusTheDuck@users.noreply.github.com>
2021-04-26 16:59:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const botao = document.querySelector('#Adicionar');
+            const texto = document.querySelector('#item');
+            const list = document.querySelector('#list');
+            const remover = document.querySelector('#remover');
+
+
+            botao.addEventListener('click', () => {
+                let inf = JSON.parse(localStorage.getItem('adicionar')) || [];
+
+                inf.push(texto.value);
+                localStorage.setItem('adicionar', JSON.stringify(inf));
+                let items = JSON.parse(localStorage.getItem('adicionar'));
+
+                list.innerHTML += `<li>
+            ${texto.value}
+            </li>`;
+                texto.value = '';
+            });
+
+            window.addEventListener('DOMContentLoaded', () => {
+                JSON.parse(localStorage.getItem('adicionar')).forEach((el) => {
+                    list.innerHTML += `
+    <li>
+        ${el}
+    </li>
+`;
+                });
+            });
+            remover.addEventListener('click', () => {
+                localStorage.clear();
+                list.innerHTML = '';
+
+                texto.value = '';
+            });
